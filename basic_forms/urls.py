19ad393db262f4e92873027d952fc628cf65b526
brian <btbell@gmail.com>
2018-08-15 00:13:46 -0400

    working with post list view

--- basic_forms/urls.py
+++ basic_forms/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', views.hello, name='hello'),
+    path('posts', views.ListPosts.as_view(), name='posts'),
 ]
--- basic_forms/views.py
+++ basic_forms/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from .models import Post
 
+# list all posts
+class ListPosts(generic.ListView):
+    model = Post
+    template_name = 'basic_forms/posts.html'
+    context_object_name = 'all_posts'
+
+    def get_queryset(self):
+        return Post.objects.all()
+
+# getting started test page
 def hello(request):
     return render(request, 'basic_forms/hello.html', {})
